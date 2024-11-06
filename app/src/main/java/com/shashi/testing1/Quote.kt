2c28981640abbf5d124a0633dca809cd55f01cb9
shashi-kant10 <shashiofficial10@gmail.com>
2024-11-07 01:18:16 +0530

    Added Model and Dao

---
+++ app/src/main/java/com/shashi/testing1/Quote.kt
@@ -0,0 +1,12 @@
+package com.shashi.testing1
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Quote(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val text: String,
+    val author: String
+)
